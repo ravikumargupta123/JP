6012db0bdad8c89561d7aa32ce62f6385c98aa88
ravi gupta <ravi.gupta@happay.in>
2019-05-27 14:31:24 +0530

    Updates

---
+++ precache-manifest.a1e0d3b8f8f26944bd9fd0e0ad648a11.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c15f2871ebcf9dbc1f4",
+    "url": "/JP/static/js/runtime~main.9c15f287.js"
+  },
+  {
+    "revision": "7d681a0007b9a29f935a",
+    "url": "/JP/static/js/main.7d681a00.chunk.js"
+  },
+  {
+    "revision": "e990ccfaedfecdf9af24",
+    "url": "/JP/static/js/1.e990ccfa.chunk.js"
+  },
+  {
+    "revision": "7d681a0007b9a29f935a",
+    "url": "/JP/static/css/main.eb8523eb.chunk.css"
+  },
+  {
+    "revision": "679200610e0e683624a50767bc9a301c",
+    "url": "/JP/index.html"
+  }
+];
